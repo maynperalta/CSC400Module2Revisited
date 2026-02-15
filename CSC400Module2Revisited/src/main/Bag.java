c1864fe656d8f21b77c83f7c5391f035c6296d8c
Maynard Peralta <maynperalta@gmail.com>
2026-02-15 12:39:39 -0700

    Improved Bag.java file for efficiency

--- CSC400Module2Revisited/src/main/Bag.java
+++ CSC400Module2Revisited/src/main/Bag.java
@@ -1,7 +1,7 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 // Generic Bag (multiset) implementation that allows duplicate elements
 class Bag<T> {
 // Using List "items" and item type "T"	
@@ -11,7 +11,11 @@ class Bag<T> {
 		items = new ArrayList<>();
 	}
 // Add an item to the Bag	
+// IMPROVEMENT: message if null value attempted.	
 	public void add(T item) {
+		if (item == null) {
+			throw new IllegalArgumentException("Null values not allowd.");
+		}
 		items.add(item);
 	}
 // Remove an item from the Bag	
@@ -41,16 +45,18 @@ class Bag<T> {
 		return items.size();
 	}
 // NEW METHOD: merge another bag into this bag	
+// IMPROVEMENT: use addAll method for efficiency and cleaner code. 	
 	public void merge(Bag<T> otherBag) {
-		for (T element : otherBag.items) {
-			items.add(element);
-		}
+		items.addAll(otherBag.items);
 	}
 // NEW METHOD: create new bag that contains only distinct items from current bag	
+// IMPROVEMENT: use Set to ensure distinct bag has no duplicates. 
 	public Bag<T> distinct() {
 		Bag<T> distinctBag = new Bag<>();
+		Set<T> seen = new HashSet<>();
+// IMPROVEMENT: if item can be added to set, add to distinct bag. 		
 		for (T element : items) {
-			if (!distinctBag.contains(element)) {
+			if (seen.add(element)) {
 				distinctBag.add(element);
 			}
 		}
